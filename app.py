514812e507e9db4a3104f954dc54cfaccc4259a7
Koval <61350874+koval01@users.noreply.github.com>
2021-07-14 14:47:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """story.q-writer.com Proxy"""
 
 from flask import request, Response, Flask
+from recaptcha_check import get_result as recaptcha_result
 from flask_cors import CORS
 from requests import post
 
@@ -10,19 +11,22 @@ CORS(app)
 
 @app.route('/', methods=['POST'])
 def _proxy(*args, **kwargs):
-    resp = post(
-        url="https://pelevin.gpt.dobro.ai/generate/",
-        headers={key: value for (key, value) in request.headers if key != 'Host'},
-        data=request.get_data(),
-        cookies=request.cookies,
-        allow_redirects=False)
+    if recaptcha_result(request.headers['re_token']):
+        resp = post(
+            url="https://pelevin.gpt.dobro.ai/generate/",
+            headers={key: value for (key, value) in request.headers if key != 'Host'},
+            data=request.get_data(),
+            cookies=request.cookies,
+            allow_redirects=False)
 
-    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    headers = [(name, value) for (name, value) in resp.raw.headers.items()
-               if name.lower() not in excluded_headers]
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items()
+                   if name.lower() not in excluded_headers]
 
-    response = Response(resp.content, resp.status_code, headers)
-    return response
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+    
+    return Response("Error", 400)
 
 
 if __name__ == '__main__':
