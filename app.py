2c1b9b44f1895c23c4ed11d8c85b288091a20f48
Koval <61350874+koval01@users.noreply.github.com>
2021-07-14 14:03:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,21 @@ from requests import post
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def _proxy(*args, **kwargs):
-    if request.method == 'POST':
-        resp = requests.post(
-            url="https://pelevin.gpt.dobro.ai/generate/",
-            headers={key: value for (key, value) in request.headers if key != 'Host'},
-            data=request.get_data(),
-            cookies=request.cookies,
-            allow_redirects=False)
+    resp = requests.post(
+        url="https://pelevin.gpt.dobro.ai/generate/",
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
 
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in resp.raw.headers.items()
-                   if name.lower() not in excluded_headers]
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
 
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    
-    return Response(
-        "Bad Request", 400
-    )
+    response = Response(resp.content, resp.status_code, headers)
+    return response
 
 
 if __name__ == '__main__':
