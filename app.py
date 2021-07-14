521b68338075c23aaab8eac63d331d8fd17e0a7c
Koval <61350874+koval01@users.noreply.github.com>
2021-07-14 13:43:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-"""Flask App Project."""
+"""story.q-writer.com Proxy"""
 
-from flask import Flask, jsonify
+from flask import request, Response
+from requests import post
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
+def _proxy(*args, **kwargs):
+    if request.method == 'POST':
+        resp = requests.post(
+            url="https://pelevin.gpt.dobro.ai/generate/",
+            headers={key: value for (key, value) in request.headers if key != 'Host'},
+            data=request.get_data(),
+            cookies=request.cookies,
+            allow_redirects=False)
+
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items()
+                   if name.lower() not in excluded_headers]
+
+        response = Response(resp.content, resp.status_code, headers)
+        return response
 
 
 if __name__ == '__main__':
